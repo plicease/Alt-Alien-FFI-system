46bfc1683f4372e12f8bd86170cd2833d09b5ff8
Graham Ollis <perl@wdlabs.com>
2015-02-16 10:50:50 -0500

    do a compile/link test at configure time

--- inc/My/MakeMaker.pm
+++ inc/My/MakeMaker.pm
@@ -1,8 +1,11 @@
 package inc::My::MakeMaker;
 
 use Moose;
+use 5.010;
 
 extends 'Dist::Zilla::Plugin::MakeMaker';
+
+my $extra;
 
 around setup_installer => sub {
   my $orig = shift;
@@ -14,11 +17,9 @@ around setup_installer => sub {
   
   my $content = $file->content;
   
-  my $extra = <<'EOF';
-my $alt = $ENV{PERL_ALT_INSTALL} || '';
-$WriteMakefileArgs{DESTDIR} =
-  $alt ? $alt eq 'OVERWRITE' ? '' : $alt : 'no-install-alt';
-EOF
+  state $extra;
+  
+  $extra = do { local $/; <DATA> } unless $extra;
   
   if($content =~ s{^WriteMakefile}{${extra}WriteMakefile}m)
   {
@@ -32,3 +33,52 @@ EOF
 };
 
 1;
+
+
+__DATA__
+do {
+
+  # we do a compile/link test
+  # but not a run test
+  # that is reserved for for t/basic.t
+
+  use lib 'lib';
+  require ExtUtils::CBuilder;
+  require Alien::FFI;
+
+  my $b = ExtUtils::CBuilder->new;
+  
+  my $obj = eval {
+    $b->compile(
+      source               => "t/basic.c",
+      extra_compiler_flags => Alien::FFI->cflags,
+    );
+  };
+  
+  if($@) {
+    print STDERR "unable to compile with cflags=", Alien::FFI->cflags, "\n";
+    exit;
+  }
+
+  my($exe, @rest) = eval {
+    $b->link_executable(
+      objects            => $obj,
+      extra_linker_flags => Alien::FFI->libs,
+    );
+  };
+  
+  if($@) {
+    print STDERR "unable to compile with libs=", Alien::FFI->libs, "\n";
+    exit;
+  }
+
+  unlink $obj if $obj;
+  unlink $exe if $exe;
+  unlink $_ for @rest;
+
+};
+
+
+my $alt = $ENV{PERL_ALT_INSTALL} || '';
+$WriteMakefileArgs{DESTDIR} =
+  $alt ? $alt eq 'OVERWRITE' ? '' : $alt : 'no-install-alt';
